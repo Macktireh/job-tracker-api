a58ac83e34b4c3e3de2a57531aea5aa0f00ca4ed
Macktireh <abdimack97@gmail.com>
2025-04-27 18:39:25 +0200

    Add custom error classes for improved error handling

---
+++ src/common/errors.ts
@@ -0,0 +1,38 @@
+import { StatusCode } from "hono/utils/http-status";
+
+export class AppError extends Error {
+  constructor(message: string, public statusCode: StatusCode) {
+    super(message);
+    this.name = this.constructor.name;
+  }
+}
+
+export class BadRequestError extends AppError {
+  constructor(message: string) {
+    super(message, 400);
+  }
+}
+
+export class UnauthorizedError extends AppError {
+  constructor(message: string) {
+    super(message, 401);
+  }
+}
+
+export class NotFoundError extends AppError {
+  constructor(message: string) {
+    super(message, 404);
+  }
+}
+
+export class ConflictError extends AppError {
+  constructor(message: string) {
+    super(message, 409);
+  }
+}
+
+export class InternalServerError extends AppError {
+  constructor(message: string) {
+    super(message, 500);
+  }
+}
